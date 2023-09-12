4c48031ee2e4493dadc1e6eb789af1a8ec42642a
czhen <56986964+shczhen@users.noreply.github.com>
2023-09-12 11:31:38 +0900

    add TIDB_SSL_MODE switch

--- src/lib/tidb.ts
+++ src/lib/tidb.ts
@@ -12,16 +12,19 @@ export function initSequelize() {
     host: process.env.TIDB_HOST || 'localhost',
     port: Number(process.env.TIDB_PORT) || 4000,
     username: process.env.TIDB_USER || 'root',
-    password: process.env.TIDB_PASSWORD || 'root',
+    password: process.env.TIDB_PASSWORD || '',
     database: process.env.TIDB_DB_NAME || 'test',
     dialectOptions: {
-      ssl: {
-        minVersion: 'TLSv1.2',
-        rejectUnauthorized: true,
-        ca: process.env.TIDB_CA_PATH
-          ? readFileSync(process.env.TIDB_CA_PATH)
-          : undefined,
-      },
+      ssl:
+        process.env?.TIDB_SSL_MODE === 'true'
+          ? {
+              minVersion: 'TLSv1.2',
+              rejectUnauthorized: true,
+              ca: process.env.TIDB_CA_PATH
+                ? readFileSync(process.env.TIDB_CA_PATH)
+                : undefined,
+            }
+          : null,
     },
   });
 }
